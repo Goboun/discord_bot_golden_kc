4685717969177dd565cb1ac1bc11ec1f75ea382b
Gob <62572362+Goboun@users.noreply.github.com>
2025-02-03 11:11:46 +0100

    Update inactive.py

--- inactive.py
+++ inactive.py
@@ -1,8 +1,7 @@
-"""
 {
   "username": "Killer Queen",
   "avatar_url": "https://static.jojowiki.com/images/thumb/d/d7/Killer_Queen_glares.png/400px-Killer_Queen_glares.png",
-  "content": "Bites Za Dusto !",
+  "content": "Bites The Dust!",
   "embeds": [
     {
       "author": {
@@ -10,9 +9,9 @@
         "url": "https://discordapp.com/channels/595785152303726612/595785152303726614",
         "icon_url": "https://pbs.twimg.com/media/Cq09NnEUkAA2fRH.jpg"
       },
-      "title": "Title",
+      "title": "A Peaceful Life",
       "url": "https://google.com/",
-      "description": "Je fais attention à ne pas me préoccuper d'ennemis, comme gagner ou perdre, qui me feraient perdre le sommeil la nuit",
+      "description": "Je fais attention à ne pas me préoccuper d'ennemis, comme gagner ou perdre, qui me feraient perdre le sommeil la nuit.",
       "color": 15258703,
       "fields": [
         {
@@ -21,17 +20,17 @@
           "inline": true
         },
         {
-          "name": "Its me Kira !",
-          "value": "You fool !",
+          "name": "It's me, Kira!",
+          "value": "You fool!",
           "inline": true
         },
         {
-          "name": "Bonjour le Golden Crew.",
-          "value": "Vous êtes bien faible..."
+          "name": "Bonjour, le Golden Crew.",
+          "value": "Vous êtes bien faibles..."
         },
         {
           "name": "Thanks!",
-          "value": "Vous m'avez montrer la vie des nuls :wink:"
+          "value": "Vous m'avez montré la vie des nuls. 😉"
         }
       ],
       "thumbnail": {
@@ -41,10 +40,9 @@
         "url": "https://pbs.twimg.com/media/Cq09NnEUkAA2fRH.jpg"
       },
       "footer": {
-        "text": "Adieu ! Signé : Goboun17",
+        "text": "Adieu! Signé : Goboun17",
         "icon_url": "https://i.imgur.com/fKL31aD.jpg"
       }
     }
   ]
 }
-"""
